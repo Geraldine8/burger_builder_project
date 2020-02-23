ea3d56a6d198b897063ac317ef00ee04ac1d9595
gerald <geraldine.antonelli@gmail.com>
2020-02-23 10:27:17 +0000

    added current_price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,7 +69,8 @@ class BurgerBuilder extends Component {
         <BuildControls
         ingredientAdded={this.addIngredientsHandler}
         ingredientRemoved={this.removeIngredientHanldler}
-        disabled={disabledInfo}/>
+        disabled={disabledInfo}
+        price={this.state.totalPrice}/>
       </Aux>
     );
   }
