1a0394355982505d24bca21bd6ac212df5046177
gerald <geraldine.antonelli@gmail.com>
2020-02-25 15:12:45 +0000

    added resposive adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,9 @@ import classes from './SideDrawer.module.css';
  const sideDrawer = (props) => {
   return (
     <div className={classes.SideDrawer}>
+    <div className={classes.Logo}>
       <Logo/>
+    </div>
       <nav>
         <NavigationItems/>
       </nav>
