a8ddb7ea74c3da174c2e80b0ba3a61a12e29fabc
gerald <geraldine.antonelli@gmail.com>
2020-03-06 16:02:05 +0000

    added order component

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import Order from '../../components/Order/Orders';
+
+class Orders Component {
+  render () {
+    return (
+
+    );
+  }
+}
+
+export default Orders;
