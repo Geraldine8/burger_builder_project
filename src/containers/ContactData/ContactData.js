b43ff686cbf9d677122d00ea19de25dacf3ab847
gerald <geraldine.antonelli@gmail.com>
2020-03-09 19:47:15 +0000

    added validation

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -16,7 +16,11 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your Name'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         street: {
           elementType: 'input',
@@ -24,7 +28,11 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Street'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false
         },
         postCode: {
           elementType: 'input',
@@ -32,7 +40,13 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Post Code'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true,
+            minLength: 5,
+            maxLength: 5
+          },
+          valid: false
         },
         country: {
             elementType: 'input',
@@ -40,7 +54,11 @@ class ContactData extends Component {
               type: 'text',
               placeholder: 'Country'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
           },
 
 
@@ -50,7 +68,11 @@ class ContactData extends Component {
               type: 'email',
               placeholder: 'Your Email'
             },
-            value: ''
+            value: '',
+            validation: {
+              required: true
+            },
+            valid: false
           },
         deliveryMethod: {
             elementType: 'select',
@@ -60,7 +82,7 @@ class ContactData extends Component {
                 {value: 'cheapest', displayValue: 'Cheapest'},
               ]
             },
-            value: ''
+            value: '',
           },
     },
     loading: false
@@ -88,6 +110,27 @@ class ContactData extends Component {
     });
   }
 
+    checkValidity (value, rules){
+      let isValid = false;
+
+      if (rules.required) {
+        // console.log(value);
+        isValid = value.trim() !== '';
+      }
+
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength;
+      }
+
+      if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength;
+      }
+
+      return isValid;
+    }
+
+
+
   inputChangedHandler = (event, inputIdentifier) => {
     // console.log(event.target.value);
     const updatedOrderForm = {
@@ -97,7 +140,9 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm});
   }
   render() {
@@ -124,6 +169,7 @@ class ContactData extends Component {
     if (this.state.loading){
       form = <Spinner/>
     }
+
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Data</h4>
