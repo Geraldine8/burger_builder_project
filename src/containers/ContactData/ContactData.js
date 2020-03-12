9698037eb9ec4850e4a7f0798785399a3b1f3a25
gerald <geraldine.antonelli@gmail.com>
2020-03-12 12:19:16 +0000

    added changes

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -88,6 +88,7 @@ class ContactData extends Component {
               ]
             },
             value: '',
+            validation: {},
             valid: true
           },
     },
@@ -119,6 +120,10 @@ class ContactData extends Component {
 
     checkValidity (value, rules){
       let isValid = true;
+
+      if (!rules) {
+        return true;
+      }
 
       if (rules.required) {
         // console.log(value);
