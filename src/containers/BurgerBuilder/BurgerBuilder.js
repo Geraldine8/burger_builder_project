ec650d68a9f3b4b1f30a0e51e709f1174fa50d60
gerald <geraldine.antonelli@gmail.com>
2020-03-12 17:47:57 +0000

    fixed order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,7 +15,6 @@ import * as actionsTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -41,7 +40,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-      this.setState({purchaseable: sum > 0})
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -84,7 +83,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchaseable={this.state.purchaseable}
+            purchaseable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}/>
         </Aux>
