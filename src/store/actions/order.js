8304205acd877941c573c1f6a63c21944ca4f6a6
gerald <geraldine.antonelli@gmail.com>
2020-03-19 16:36:05 +0000

    fixed Orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -41,3 +41,45 @@ export const purchaseInit = () => {
     type: actionTypes.PURCHASE_INIT
   }
 }
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdesStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+//asyn
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdesStart());
+    axios.get('/orders.json')
+    .then(res => {
+
+      const fetchOrders = [];
+      for (let key in res.data) {
+        fetchOrders.push({
+          ...res.data[key],
+          id: key
+        });
+      }
+      dispatch(fetchOrdersSuccess(fetchOrders));
+    })
+    .catch(err => {
+      dispatch(fetchOrdersFail(err));
+    });
+  }
+};
