ca599c4ff895be7dd8fef8d6ab3c7a56f4f5998d
gerald <geraldine.antonelli@gmail.com>
2020-03-21 08:20:51 +0000

    added auth

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -115,7 +115,6 @@ class ContactData extends Component {
 
   checkValidity (value, rules) {
     let isValid = true;
-
     if (!rules) {
       return true;
     }
@@ -133,10 +132,18 @@ class ContactData extends Component {
       isValid = value.length <= rules.maxLength && isValid;
     }
 
+    if (rules.isEmail) {
+         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+         isValid = pattern.test(value) && isValid
+     }
+
+     if (rules.isNumeric) {
+         const pattern = /^\d+$/;
+         isValid = pattern.test(value) && isValid
+     }
+
     return isValid;
   }
-
-
 
   inputChangedHandler = (event, inputIdentifier) => {
     // console.log(event.target.value);
