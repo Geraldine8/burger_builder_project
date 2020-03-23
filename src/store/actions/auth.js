6f61713799fd7b74403dc5a36ac0d11b6b697bc0
gerald <geraldine.antonelli@gmail.com>
2020-03-23 14:31:04 +0000

    added Logout link

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -27,7 +27,7 @@ export const authFail = (error) => {
 export const logout = () => {
   return {
     type: actionTypes.AUTH_LOGOUT
-  }
+  };
 };
 
 export const checkoutTimeout = (expirationTime) => {
