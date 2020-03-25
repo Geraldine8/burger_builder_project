fe39cd3950658f5c4b9cfebbec2e0c244ec01c55
gerald <geraldine.antonelli@gmail.com>
2020-03-25 15:54:49 +0000

    fixed the redirection to the frontpage

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ import Aux from '../../../hoc/Aux/Aux';
   return (
     <Aux>
     <Backdrop show={props.open} clicked={props.closed}/>
-      <div className={attachedClasses.join(' ')}>
+      <div className={attachedClasses.join(' ')} onClick={props.closed}>
         <div className={classes.Logo}>
           <Logo/>
         </div>
