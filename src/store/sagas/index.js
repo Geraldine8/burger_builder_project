9a37d1f435272070c36311277e65d0a108122695
gerald <geraldine.antonelli@gmail.com>
2020-04-03 16:15:38 +0100

    added checkouttime saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,9 +1,10 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import {logoutSaga} from './auth';
+import {logoutSaga, checkoutTimeoutSaga} from './auth';
 
 
 export function* watchAuth() {
+  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_INITITATE_LOGOUT, logoutSaga);
 }
