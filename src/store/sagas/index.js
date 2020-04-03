ccbd284017cde9ea845d10dac06da927f3c8c692
gerald <geraldine.antonelli@gmail.com>
2020-04-03 20:39:42 +0100

    added burgerBuilder saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,11 +1,18 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import {logoutSaga, checkoutTimeoutSaga, authUserSaga} from './auth';
+
+import {logoutSaga, checkoutTimeoutSaga, authUserSaga, authCheckStateSaga} from './auth';
+import {initIngredientsSaga} from './burgerBuilder';
 
 
 export function* watchAuth() {
   yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkoutTimeoutSaga);
   yield takeEvery(actionTypes.AUTH_INITITATE_LOGOUT, logoutSaga);
   yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+}
+
+export function* watchBugerBuilder() {
+  yield takeEvery(actionTypes.INIT_INGREDIENTS, initIngredientsSaga);
 }
